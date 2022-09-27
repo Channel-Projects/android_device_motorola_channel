312ef449b9ace53de41af1860bb23271b143201c
Marc Bourgoin <themard85@gmail.com>
2022-09-26 21:03:04 -0600

    channel: Adapt to TWRP 12.1
    Change-Id: I78b0497bd33da3e71368f7e7ad58e17b1d91644d

--- device.mk
+++ device.mk
@@ -65,9 +65,6 @@ PRODUCT_PACKAGES += \
     bootctrl.$(PRODUCT_PLATFORM) \
     bootctrl.$(PRODUCT_PLATFORM).recovery
 
-PRODUCT_HOST_PACKAGES += \
-    libandroidicu
-
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
@@ -81,10 +78,6 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
     ro.bootimage.build.date.utc \
     ro.build.date.utc
-
-# Apex libraries
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.security_patch=2099-12-31
